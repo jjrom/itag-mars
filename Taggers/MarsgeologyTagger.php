5afb7dce707ba50b3e5e49a289016ff371f78bd7
jjrom <jerome.gasperi@gmail.com>
2021-10-26 17:19:32 +0200

    [NOT WORKING] Intermediate commit

---
+++ Taggers/MarsgeologyTagger.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Copyright 2021 Jérôme Gasperi
+ *
+ * Licensed under the Apache License, version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+class MarsgeologyTagger extends GenericTagger
+{
+
+    /*
+     * This Tagger is specific to Mars
+     */
+    public $planet = 'mars';
+    
+    /*
+     * Data references
+     */
+    public $references = array(
+        array(
+            'dataset' => 'Mars 15M Geologic Map GIS Renovation',
+            'publisher' => 'USGS Astrogeology Science Center',
+            'author' => 'James A. Skinner, Jr.',
+            'originator' => 'Trent Hare, Ken Tanaka',
+            'description' => 'A digital adaptation of the hard-copy Viking Orbiter-based geologic maps of Mars. The western equatorial region was originally mapped by David H. Scott and K. L. Tanaka (USGS I-1802-A, 1986, 1:15M scale). The eastern equatorial region was originally mapped by Ronald Greeley and J. E. Guest (USGS I-1802-B, 1987, 1:15M scale). The north and south polar regions were originally mapped by K. L. Tanaka and D. H. Scott (USGS I-1802-C, 1987, 1:15M scale). A conference abstract submitted to the 37th Lunar and Planetary Science Conference outlines and discusses the rationale and methodology for the digitized version presented herein. The abstract reference is: Skinner, J. A., Jr, T. M. Hare, and K. L. Tanaka 2006, LPSC XXXVII, abstract #2331.',
+            'modified' => '3 June 2019',
+            'license' => 'Free of charge',
+            'url' => 'https://astrogeology.usgs.gov/search/map/Mars/Geology/Mars15MGeologicGISRenovation'
+        )
+    );
+    
+    /*
+     * Columns mapping per table
+     */
+    protected $columnsMapping = array(
+        'geo_units_oc_dd' => array(
+            'name' => 'unitname',
+            'symbol' => 'unitsymbol'
+        )
+    );
+    
+    /**
+     * Constructor
+     *
+     * @param DatabaseHandler $dbh
+     * @param array $config
+     */
+    public function __construct($dbh, $config)
+    {
+        parent::__construct($dbh, $config);
+    }
+    
+    /**
+     * Tag metadata
+     *
+     * @param array $metadata
+     * @param array $options
+     * @return array
+     * @throws Exception
+     */
+    public function tag($metadata, $options = array())
+    {
+        $rawResults = parent::tag($metadata, array_merge($options, array(
+            'schema' => 'mars',
+            'computeArea' => true)
+        ));
+        
+        // Now sum up the same classes
+        return $this->getGeology($rawResults);
+    }
+
+    /**
+     * Return a clean geology tagging
+     * 
+     * @param array $rawResults
+     */
+    private function getGeology($rawResults)
+    {
+        
+        $geology = array();
+        foreach ($sums as $key => $val) {
+            $pcover = $this->percentage($this->toSquareKm($val), $this->area);
+            if ($val !== 0 && $pcover > 0) {
+                $name = $this->clcClassNames[$key] ?? 'unknown';
+                array_push($landCover, array(
+                    'name' => $name,
+                    'id' => 'landcover'. iTag::TAG_SEPARATOR . strtolower($name),
+                    'area' => $this->toSquareKm($val),
+                    'pcover' => $pcover
+                ));
+            }
+        }
+
+        return $landCover;
+    }
+
+
+}
---
+++ installMarsDatasources.sh
@@ -0,0 +1,100 @@
+#! /bin/bash
+#
+# Copyright 2018 Jérôme Gasperi
+#
+# Licensed under the Apache License, version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Force script to exit on error
+set -e
+
+ENV_FILE=__NULL__
+DATA_DIR=$(pwd)"/data"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+ENCODING=UTF8
+
+function showUsage {
+    echo ""
+    echo "   Install itag mars datasources "
+    echo ""
+    echo "   Usage $0 -e config.env"
+    echo ""
+    echo "      -e | --envfile iTag environnement file (see https://github.com/jjrom/itag/blob/master/config.en)"
+    echo "      -d | --dataDir Directory containing datasources (default is $(pwd)/data)"
+    echo "      -h | --help show this help"
+    echo ""
+}
+
+# Parsing arguments
+while [[ $# > 0 ]]
+do
+	key="$1"
+	case $key in
+        -e|--envfile)
+            ENV_FILE="$2"
+            shift # past argument
+            ;;
+        -d|--dataDir)
+            DATA_DIR="$2"
+            shift # past argument
+            ;;
+        -h|--help)
+            showUsage
+            exit 0
+            shift # past argument
+            ;;
+            *)
+        shift # past argument
+        # unknown option
+        ;;
+	esac
+done
+
+if [ ! -f ${ENV_FILE} ]; then
+    showUsage
+    echo -e "${RED}[ERROR]${NC} Missing or invalid config file!"
+    echo ""
+    exit 0
+fi
+
+if [ ! -d "${DATA_DIR}"]; then
+    showUsage
+    echo -e "${RED}[ERROR]${NC} You must specify a data directory!"
+    echo ""
+    exit 0
+fi
+
+# Source config file
+. ${ENV_FILE}
+
+if [ "${ITAG_DATABASE_HOST}" == "itagdb" ] || [ "${ITAG_DATABASE_HOST}" == "host.docker.internal" ]; then
+    DATABASE_HOST_SEEN_FROM_DOCKERHOST=localhost
+else
+    DATABASE_HOST_SEEN_FROM_DOCKERHOST=${ITAG_DATABASE_HOST}
+fi
+
+echo -e "[INFO] Using ${DATA_DIR} directory"
+
+echo -e "[INFO] Creating mars schema in database"
+PGPASSWORD=${ITAG_DATABASE_USER_PASSWORD} psql -U ${ITAG_DATABASE_USER_NAME} -d ${ITAG_DATABASE_NAME} -h ${DATABASE_HOST_SEEN_FROM_DOCKERHOST} -p ${ITAG_DATABASE_EXPOSED_PORT} > /dev/null 2>errors.log << EOF
+CREATE SCHEMA IF NOT EXISTS mars;
+EOF
+
+SHP2PGSQL="docker run --rm -v ${DATA_DIR}:/data:ro jjrom/shp2pgsql"
+
+# ================================================================================
+echo -e "[INFO] Install Mars 15M Geologic Map GIS Renovation from [USGS]"
+${SHP2PGSQL} -g geom -d -W ${ENCODING} -s 4326 -I /data/I1802ABC_Mars2000_Sphere/geo_units_oc_dd.shp mars.geo_units_oc_dd 2> /dev/null | PGPASSWORD=${ITAG_DATABASE_USER_PASSWORD} psql -U ${ITAG_DATABASE_USER_NAME} -d ${ITAG_DATABASE_NAME} -h ${DATABASE_HOST_SEEN_FROM_DOCKERHOST} -p ${ITAG_DATABASE_EXPOSED_PORT} > /dev/null 2>errors.log
+echo -e "${GREEN}[INFO] Mars 15M Geologic Map GIS Renovation installed${NC}"
